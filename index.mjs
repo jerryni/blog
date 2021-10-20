7f2a1f8b7884e54e70b6386c24b076faac401c5f
nirizhe <nirizhe@youzan.com>
2021-10-20 20:14:49 +0800

    feat: send mail

--- index.mjs
+++ index.mjs
@@ -1,33 +1,42 @@
 import fetch from 'node-fetch';
+import { sendMail } from './mail.mjs';
 
-console.log('process.env.ACCESS_TOKEN:', process.env.ACCESS_TOKEN)
+async function start() {
+  fetch("https://www.ems.com.cn/apple/getMailNoRoutes", {
+    "headers": {
+      "accept": "application/json, text/javascript, */*; q=0.01",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6,und;q=0.5",
+      "cache-control": "no-cache",
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-requested-with": "XMLHttpRequest",
+      "Referer": "https://www.ems.com.cn/apple/items?mailNo=EZ334555634CN",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "mailNum=EZ334555634CN",
+    "method": "POST"
+  }).then((res) => res.json())
+  .then(async (res) => {
+    const item = res.trails[0][0];
 
-fetch("https://www.ems.com.cn/apple/getMailNoRoutes", {
-  "headers": {
-    "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6,und;q=0.5",
-    "cache-control": "no-cache",
-    "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "x-requested-with": "XMLHttpRequest",
-    "Referer": "https://www.ems.com.cn/apple/items?mailNo=EZ334555634CN",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-  },
-  "body": "mailNum=EZ334555634CN",
-  "method": "POST"
-}).then((res) => res.json())
-.then((res) => {
-  const item = res.trails[0][0];
+    if (Math.floor((Date.now() - new Date(item.optime)) / 1000 / 60) < 30) {
+      await sendMail({
+        subject: '您的iphone到这了',
+        html: `${item.processingInstructions} 正在 ${item.opreateType} <br/> <pre>${JSON.stringify(item)}</pre>`
+      })
+    }
 
-  if (Math.floor((Date.now() - new Date(item.optime)) / 1000 / 60) < 30) {
-    console.log('item', item);
-  }
+    await sendMail({
+      subject: 'test',
+      html: `xxx`
+    })
+  });
+}
 
-  console.log('最新更新时间：', item.optime);
-});
+await start();
---
+++ mail.mjs
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+// 使用async..await 创建执行函数
+export async function sendMail({
+  subject,
+  html,
+}) {
+  // 如果你没有一个真实邮箱的话可以使用该方法创建一个测试邮箱
+  // let testAccount = await nodemailer.createTestAccount();
+
+  // 创建Nodemailer传输器 SMTP 或者 其他 运输机制
+  let transporter = nodemailer.createTransport({
+    host: "smtp.163.com", // 第三方邮箱的主机地址
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'z308289891@163.com', // 发送方邮箱的账号
+      pass: process.env.ACCESS_TOKEN, // 邮箱授权密码
+    },
+  });
+
+  // 定义transport对象并发送邮件
+  let info = await transporter.sendMail({
+    from: 'z308289891@163.com', // 发送方邮箱的账号
+    to: "308289891@qq.com", // 邮箱接受者的账号
+    subject: subject || "默认标题" , // Subject line
+    text: "From Github Zhe", // 文本内容
+    html: html || "<b>无主题</b>", // html 内容, 如果设置了html内容, 将忽略text内容
+  });
+}
